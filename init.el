2d26fb3202f2a5bae9a5ec4a12ef7afa32f86a9a
CoreyCole <coreyleoc@gmail.com>
2016-06-02 12:40:25 -0700

    first commit

---
+++ init.el
@@ -0,0 +1,13 @@
+(global-set-key (kbd "<f12>") (lambda () (interactive) (find-file-other-window user-init-file)))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
